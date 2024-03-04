cc77258f62f64a5cafdba8fb617600f99ba06f2a
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-03-03 20:33:19 -0800

    Testing part A

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-'''Exectuion Command: spark-submit TitleCountSpark.py stopwords.txt delimiters.txt dataset/titles/ dataset/output'''
-
 import sys
 from pyspark import SparkConf, SparkContext
 
@@ -9,10 +7,10 @@ stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
 with open(stopWordsPath) as f:
-	#TODO
+    stopWords = set(f.read().splitlines())
 
 with open(delimitersPath) as f:
-    #TODO
+    delimiters = f.read().strip()
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
@@ -20,11 +18,28 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
 
-#TODO
+def split_line(line):
+    for delimiter in delimiters:
+        line = line.replace(delimiter, ' ')
+    return line.split()
+
+def filter_stop_words(word):
+    return word.lower() not in stopWords
+
+wordCounts = lines.flatMap(split_line) \
+    .filter(filter_stop_words) \
+    .map(lambda word: (word.lower(), 1)) \
+    .reduceByKey(lambda a, b: a + b) \
+    .map(lambda x: (x[1], x[0])) \
+    .sortByKey(False)
+
+# Collect the results
+results = wordCounts.collect()
 
 outputFile = open(sys.argv[4],"w")
 
-#TODO
-#write results to output file. Foramt for each line: (line +"\n")
+for count, word in results:
+    outputFile.write(f"{word}\t{count}\n")
 
+outputFile.close()
 sc.stop()
