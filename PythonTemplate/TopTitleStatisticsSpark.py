ffa5f177212facf86a841c08729a7a658d4eecb2
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-03-03 22:01:30 -0800

    Fixing part B

--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -9,22 +9,21 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1], 1)
 
 def parser(line):
-    line = line.strip()
-    title, count = line.split('\t', 1)
-    return count
+    count = line.split('\t')[1]
+    return int(count)
 
 attributes = lines.map(parser)
 sum_val = attributes.reduce(lambda a, b: a + b)
 count = attributes.count()
-mean_val = sum_val / count
+mean_val = int(sum_val) / count
 min_val = attributes.min()
 max_val = attributes.max()
-variance = attributes.map(lambda x: (x - mean_val) ** 2).reduce(lambda a, b: a + b) / count
+variance = float(attributes.map(lambda x: (x - mean_val) ** 2).reduce(lambda a, b: a + b)) / count
 
 outputFile = open(sys.argv[2], "w")
 
 outputFile.write('Mean\t%s\n' % int(mean_val))
-outputFile.write('Sum\t%s\n' % sum_val)
+outputFile.write('Sum\t%s\n' % int(sum_val))
 outputFile.write('Min\t%s\n' % min_val)
 outputFile.write('Max\t%s\n' % max_val)
 outputFile.write('Var\t%s\n' % int(variance))
