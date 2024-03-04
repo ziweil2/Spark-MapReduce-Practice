4bee08e66e1d643f94da4f055a6861b036974e3d
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-03-04 00:05:47 -0800

    Fixing part E

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -7,8 +7,6 @@ conf = SparkConf().setMaster("local").setAppName("PopularityLeague")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile(sys.argv[1], 1) 
-
 def parser(line):
     line = line.split(':')
     page = line[0].strip()
@@ -16,22 +14,32 @@ def parser(line):
     return [(link, 1) for link in links if link != page]
 
 linkCounts = lines.flatMap(parser)
-popularityCounts = linkCounts.reduceByKey(lambda a, b: a + b)
+popularityCounts = linkCounts.reduceByKey(lambda a, b: a + b).sortBy(lambda x: (x[1], x[0]))
 
-leagueIds = sc.textFile(sys.argv[2], 1)
-leaguePages = popularityCounts.filter(lambda x: x[0] in leagueIds)
+leagueIds = sc.textFile(sys.argv[2], 1).collect()
+leagueCounts = popularityCounts.filter(lambda x: x[0] in leagueIds).collect()
 
-sortedLeaguePages = leaguePages.sortBy(lambda x: (x[1], x[0]))
+currentRank = 0
+currentCount = 0
+counter = 0
+pageRanks = []
 
-pageRanks = sortedLeaguePages.zipWithIndex().map(lambda x: (x[0][0], x[1] + 1))
+for page, count in leagueCounts:
+    if count > currentCount and counter > 0:
+        currentRank = counter
+        pageRanks.append((page, currentRank))
+    else:
+        pageRanks.append((page, currentRank))
 
-result = pageRanks.sortBy(lambda x: x[0])
+    currentCount = count
+    counter += 1
+
+pageRanks.sort(key=lambda x: x[0])
 
 output = open(sys.argv[3], "w")
 
 # write results to output file. Foramt for each line: (key + \t + value +"\n")
-for page, rank in result:
+for page, rank in pageRanks:
     output.write(f"{page}\t{rank}\n")
 
 sc.stop()
-
