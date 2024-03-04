9e382f98be803ab31b8aee337206ddfd20af8ecd
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-03-03 23:02:33 -0800

    Testing part D

--- PythonTemplate/TopPopularLinksSpark.py
+++ PythonTemplate/TopPopularLinksSpark.py
@@ -8,12 +8,22 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+def parser(line):
+    line = line.split(':')
+    page = line[0].strip()
+    links = line[1].strip().split() if len(line) > 1 else []
+    return (page, links)
+
+parsedLines = lines.map(parser)
+linkCounts = parsedLines.flatMap(lambda x: [(link,1) for link in x[1] if link != x[0]]) \
+                        .reduceByKey(lambda a, b: a + b)
+
+topLinks = linkCounts.sortBy(lambda x: x[1], ascending=False).take(10)
+result = sorted(topLinks, key=lambda x: (x[1], x[0]))
 
 output = open(sys.argv[2], "w")
-
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+for link, count in result:
+    output.write(f"{link}\t{count}\n")
 
 sc.stop()
 
