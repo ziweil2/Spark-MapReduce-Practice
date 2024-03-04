49fb54f64cb0cd00a63acb011dd78de846cc2d32
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-03-03 22:31:53 -0800

    Testing part C

--- PythonTemplate/OrphanPagesSpark.py
+++ PythonTemplate/OrphanPagesSpark.py
@@ -8,12 +8,27 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+def parser(line):
+    line = line.split(':')
+    page = line[0].strip()
+    links = [link.strip() for link in line[1].split() if len(line) > 1 and line[1].strip() != "" and link.strip() != 
+page]
+    return (page, links)
+
+parsedLines = lines.map(parser)
+
+pages = parsedLines.map(lambda x: (x[0], 0))
+references = parsedLines.flatMap(lambda x: x[1])
+referenced = references.map(lambda x: (x, 1))
+
+orphanPages = pages.union(referenced).reduceByKey(lambda a,b: a + b).filter(lambda x: x[1] == 0)
+
+# Collect the orphan page IDs
+orphans = orphanPages.sortByKey().collect()
 
 output = open(sys.argv[2], "w")
-
-#TODO
-#write results to output file. Foramt for each line: (line + "\n")
+for orphan in orphans:
+    output.write(f"{orphan}\n")
 
 sc.stop()
 
